bcbabd805f6f6f79c7118ae3b52f5f374921320c
Manahil40 <bitf19a040@pucit.edu.pk>
2023-01-18 01:52:14 +0500

    Listener created

--- app/src/main/java/com/example/app_db/MainActivity2.java
+++ app/src/main/java/com/example/app_db/MainActivity2.java
@@ -2,12 +2,31 @@ package com.example.app_db;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    ImageButton whatsapp, github, result;
+    TextView wapp, hub, res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        whatsapp = (ImageButton) findViewById(R.id.whatappButton);
+        whatsapp.setOnClickListener((View.OnClickListener) this);
+
+        github = (ImageButton) findViewById(R.id.githubButton);
+        github.setOnClickListener((View.OnClickListener) this);
+
+        result = (ImageButton) findViewById(R.id.resultButton);
+        result.setOnClickListener((View.OnClickListener) this);
+
+
+
     }
 }
